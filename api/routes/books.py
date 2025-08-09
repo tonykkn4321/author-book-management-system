ae271fb24cf6665454acf95a0321c2bbb1a8a6d3
Tony Kwong <tonykkn4321@gmail.com>
2025-08-09 09:50:37 +0800

    Created books GET, PUT, PATCH, DELETE endpoints

--- api/routes/books.py
+++ api/routes/books.py
@@ -39,3 +39,47 @@ def create_book():
     except Exception as e:
         print(f"Error creating book: {e}")
         return response_with(resp.INVALID_INPUT_422)
+
+#   GET books endpoint
+@book_routes.route('/', methods=['GET'])
+def get_book_list():
+    fetched = Book.query.all()
+    book_schema = BookSchema(many=True, only=['author_id','title', 'year', 'id'])
+    books = book_schema.dump(fetched)
+    return response_with(resp.SUCCESS_200, value={"books": books})
+
+#   PUT books endpoint
+@book_routes.route('/<int:id>', methods=['PUT'])
+def update_book_detail(id):
+    data = request.get_json()
+    get_book = Book.query.get_or_404(id)
+    get_book.title = data['title']
+    get_book.year = data['year']
+    db.session.add(get_book)
+    db.session.commit()
+    book_schema = BookSchema()
+    book = book_schema.dump(get_book)
+    return response_with(resp.SUCCESS_200, value={"book": book})
+
+#   PATCH books endpoint
+@book_routes.route('/<int:id>', methods=['PATCH'])
+def modify_book_detail(id):
+    data = request.get_json()
+    get_book = Book.query.get_or_404(id)
+    if data.get('title'):
+        get_book.title = data['title']
+    if data.get('year'):
+        get_book.year = data['year']
+    db.session.add(get_book)
+    db.session.commit()
+    book_schema = BookSchema()
+    book = book_schema.dump(get_book)
+    return response_with(resp.SUCCESS_200, value={"book": book})
+
+#   DELETE books endpoint
+@book_routes.route('/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    get_book = Book.query.get_or_404(id)
+    db.session.delete(get_book)
+    db.session.commit()
+    return response_with(resp.SUCCESS_204)
