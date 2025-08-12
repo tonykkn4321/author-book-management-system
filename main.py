7f44262cf5f8f84d101f1b6e46b7df2447f81965
Tony Kwong <tonykkn4321@gmail.com>
2025-08-12 09:40:48 +0800

    Modified main.py

--- main.py
+++ main.py
@@ -68,4 +68,4 @@ app = create_app()
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()  # Safe to run locally
-    app.run(port=5000, host="0.0.0.0", use_reloader=False)
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
