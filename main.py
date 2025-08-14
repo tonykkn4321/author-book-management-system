ab6d3744b5e9b70f1e12cb3bf4633b5ef264fb5d
Tony Kwong <tonykkn4321@gmail.com>
2025-08-14 09:46:20 +0800

    Added PATCH in access-Control-Allow-Methods header

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def create_app():
             response.headers["Access-Control-Allow-Origin"] = origin
             response.headers["Access-Control-Allow-Credentials"] = "true"
         response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
-        response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
+        response.headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,PATCH,DELETE,OPTIONS"
         return response
 
     @app.errorhandler(400)
